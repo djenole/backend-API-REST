1f52abd41954773c03f51e69fb066cf59c31ebfe
Djenole da Silva <djenole@gmail.com>
2025-05-12 13:07:09 -0300

    HttpStatus adicionado em createTransaction de controller para resposta de status

--- springboot/src/main/java/backendAPI/springboot/controller/TransactionController.java
+++ springboot/src/main/java/backendAPI/springboot/controller/TransactionController.java
@@ -2,7 +2,9 @@ package backendAPI.springboot.controller;
 
 import java.time.OffsetDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,12 @@ public class TransactionController {
             return ResponseEntity.unprocessableEntity().build();
         }
         transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransactions() {
+        transactionService.clearTransactions();
         return ResponseEntity.ok().build();
     }
 }
